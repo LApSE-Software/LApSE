5b7a6f41113319aee95b55a54effd5989e6e71f4
burhanloey <burhan_loey@yahoo.com.my>
2015-07-30 16:26:51 +0800

    Add License Headers

--- src/drawingtagger/view/RootLayoutController.java
+++ src/drawingtagger/view/RootLayoutController.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Burhanuddin.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package drawingtagger.view;
 
 import drawingtagger.util.FileChooserType;
@@ -220,6 +243,8 @@ public class RootLayoutController implements Initializable {
                 }
             } catch (IOException ex) {
                 Logger.getLogger(RootLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                Logger.getLogger(RootLayoutController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -227,7 +252,7 @@ public class RootLayoutController implements Initializable {
     /**
      * Load program after loading file.
      */
-    private void loadProgram() {
+    private void loadProgram() throws Exception {
         clearData();
         loadDataFromTemporaryData();
         FXCollections.sort(mainApp.getTaggedLines());
@@ -242,14 +267,19 @@ public class RootLayoutController implements Initializable {
      * afterwards.
      */
     private void loadDataFromTemporaryData() {
-        mainApp.getBeforeLines().addAll(tempDataHolder.retrieveTempData("beforeLines"));
-        mainApp.getAfterLines().addAll(tempDataHolder.retrieveTempData("afterLines"));
-        tempDataHolder.retrieveTempData("taggedLines").stream()
-                .map((line) -> loadLineFromString(line))
-                .forEach((taggedLine) -> {
-            mainApp.getTaggedLines().add(taggedLine);
-        });
-        tempDataHolder.clearAll();
+        if (!tempDataHolder.isEmpty("beforeLines")) {
+            mainApp.getBeforeLines().addAll(tempDataHolder.retrieveTempData("beforeLines"));
+        }
+        if (!tempDataHolder.isEmpty("afterLines")) {
+            mainApp.getAfterLines().addAll(tempDataHolder.retrieveTempData("afterLines"));
+        }
+        if (!tempDataHolder.isEmpty("taggedLines")) {
+            tempDataHolder.retrieveTempData("taggedLines").stream()
+                    .map((line) -> loadLineFromString(line))
+                    .forEach((taggedLine) -> {
+                mainApp.getTaggedLines().add(taggedLine);
+            });
+        }
     }
     
     /**
